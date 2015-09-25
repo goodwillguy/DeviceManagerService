b6199118c409e0480debb555902708aee1de4ed7
goodwillguy <goodwillguy@gmail.com>
2015-09-25 10:52:35 +1000

    added new dtos items

---
+++ Parcel.Common/Dto/ParcelTransactionDto.cs
@@ -0,0 +1,19 @@
+﻿using Common.Values;
+using System;
+
+namespace Parcel.Common.Dto
+{
+    public class ParcelTransactionDto:Base
+    {
+        public Guid ParcelTransactionId { get; set; }
+
+        public Guid ParcelId { get; set; }
+
+        public TransactionState TransactionState { get; set; }
+
+        public Guid DropOffAgentId { get; set; }
+
+        public DateTime DropoffTime { get; set; }
+        public Guid LockerId { get; set; }
+    }
+}
